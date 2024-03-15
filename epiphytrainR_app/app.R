20473dee566f7a707d72e15b8d58443d990f84f5
PaulMelloy <paul@melloy.com.au>
2024-03-15 12:58:47 +1000

    add tab 3 SEIR

--- epiphytrainR_app/app.R
+++ epiphytrainR_app/app.R
@@ -2,6 +2,12 @@ library(shiny)
 library(shinythemes)
 #library(htmltools)
 library(shinycssloaders)
+if(isFALSE("showPageSpinner" %in% ls("package:shinycssloaders"))){
+   # showPageSpinner is not in the CRAN package version but on github
+   detach("package:shinycssloaders", unload = TRUE)
+   remotes::install_github("daattali/shinycssloaders")
+   library(shinycssloaders)
+}
 library(epiphytoolR)
 library(epicrop)
 library(tidyr)
@@ -15,14 +21,12 @@ load("data/g_weather.rda")
 #                      dates = c("2010-01-01", "2023-12-31"))
 # save(g_weather, file = "epiphytrainR_app/data/g_weather.rda")
 
-
-
 # Define UI for application that draws a histogram
 ui <-
    fluidPage(
       theme = shinytheme("darkly"),
       # --------------------  Tab 1 --------------------------
-      tabsetPanel(
+      tabsetPanel(id = "tabs",
          tabPanel(
             "Start page",
             h1("epiphytrainR"),
@@ -43,7 +47,9 @@ ui <-
             "using packages such as:", a("epicrop", href = "http://adamhsparks.github.io/epicrop/index.html"),
             " developed by", a("Prof. Adam Sparks", href = "https://adamhsparks.netlify.app/")
          ),
-      # --------------------  Tab 2 --------------------------
+
+
+         # --------------------  Tab 2 --------------------------
       tabPanel("Epicrop",
                p("Epicrop is a package for predicting the intensity of rice diseases using weather data."),
                sidebarLayout(
@@ -55,8 +61,7 @@ ui <-
                                      "Brown Spot" = "bs",
                                      "Leaf Blast" = "lb",
                                      "Sheath Blight" = "sb",
-                                     "Rice Tungro Spherical Virus" = "tu",
-                                     "Custom" = "custom"
+                                     "Rice Tungro Spherical Virus" = "tu"
                                      )),
                      uiOutput("custom_disease"),
                      dateInput(
@@ -76,13 +81,118 @@ ui <-
                   # Main panel
                   mainPanel(
                      h2("Weather plot"),
-                     plotOutput("weather_plot"),
+                     plotOutput("weather_plot1"),
                      p(),
                      h2("Disease progress"),
-                     plotOutput("disease_plot")
-                  )
-               )))
-      )
+                     plotOutput("disease_plot1")
+                  ) # end mainPanel
+               )# end sidebarLayout
+               ), # end tabPanel
+      #
+      #
+      # # --------------------  Tab 3 --------------------------
+      tabPanel("SEIR", id = "SEIR",
+               p("SEIR is a disease model which simulates Susceptible, Exposed,
+                 Infectious and Removed plant sites during a plant disease epidemic."),
+                sidebarLayout(
+                   sidebarPanel(
+                     dateInput(
+                        "startdate",
+                        "Date of rice crop emergence",
+                        value = "2023-03-19",
+                        min = "2010-01-01",
+                        max = "2023-10-30"),
+                     hr(),
+                     sliderInput("onset",
+                                 "Onset of disease (days after emergence)",
+                                 min = 1,
+                                 max = 120,
+                                 value = 30),
+                     hr(),
+                     sliderInput("H0",
+                                 "Initial Heathy sites",
+                                 min = 1,
+                                 max = 1000,
+                                 value = 100),
+                     hr(),
+                     sliderInput("I0",
+                                 "Initial infectious sites",
+                                 min = 1,
+                                 max = 120,
+                                 value = 1),
+                     hr(),
+                     sliderInput("Tm_optim",
+                                 "Optimum temperature for disease (C)",
+                                 min = 0,
+                                 max = 45,
+                                 value = 24),
+                     hr(),
+                     sliderInput("RH_optim",
+                                 "Optimum Relative humidity for disease %",
+                                 min = 30,
+                                 max = 100,
+                                 value = 90),
+                     hr(),
+                     sliderInput("RcOpt",
+                                 "Infection rate, sites per day",
+                                 min = 0,
+                                 max = 1,
+                                 value = 0.5,
+                                 step = 0.01),
+                     hr(),
+                     sliderInput("latent_period",
+                                 "Latent period (days)",
+                                 min = 2,
+                                 max = 30,
+                                 value = 5),
+                     hr(),
+                     sliderInput("infectious_period",
+                                 "Infectious period (days)",
+                                 min = 0,
+                                 max = 100,
+                                 value = 30),
+                     hr(),
+                     sliderInput("aggregation",
+                                 "Aggregation coeficient",
+                                 min = 1,
+                                 max = 20,
+                                 value = 2.5,
+                                 step = 0.1),
+                     hr(),
+                     sliderInput("RRS",
+                                 "Relative rate of senecence",
+                                 min = 0,
+                                 max = 0.2,
+                                 value = 0.005,
+                                 step = 0.001),
+                     hr(),
+                     sliderInput("RRG",
+                                 "Relative rate of Growth",
+                                 min = 0,
+                                 max = 1,
+                                 value = 0.2,
+                                 step = 0.01),
+                     hr(),
+                     p(),
+                     h3("Acknowledgement"),
+                     "SEIR R model was authored by",
+                     a("Prof. Adam Sparks", href = "https://adamhsparks.netlify.app/"),
+                     "find the SEIR model in the epicrop package on",
+                     a("github", href = "https://github.com/adamhsparks/epicrop"),
+                     "or", a("codeburg", href = "https://codeberg.org/adamhsparks/epicrop"),
+                     uiOutput("testing")
+                ), # end sidebarPanel
+                  mainPanel(
+                     h2("Weather plot"),
+                     plotOutput("weather_plot2"),
+                     p(),
+                     h2("Disease progress"),
+                     plotOutput("disease_plot2")
+                   ) # end mainPanel
+                ) # end sidebarLayout
+                ) # end tabPanel
+      ) # end tabsetPanel
+      ) # end fluidPage
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -102,8 +212,7 @@ server <- function(input, output) {
                     layerId = "gatton_start")
    })
 
-   # Update the weather location map if either the map is clicked or, update
-   #   Delivery button is clicked
+   # Update the weather location map if either the map is clicked
    observeEvent(
       input$worldmap_click, {
          cat(input$worldmap_click[["lat"]]," ", input$worldmap_click[["lng"]])
@@ -129,9 +238,33 @@ server <- function(input, output) {
       return(got_wth)}
       })
 
-
    epicrop_out <-
       reactive({
+         if(input$tabs == "SEIR"){
+            epi_out <-
+               SEIR(
+               wth = weather_dat(),
+               emergence = input$startdate,
+               onset = input$onset,
+               duration = 120L,
+               rhlim = 90L,
+               rainlim = 5L,
+               H0 = input$H0,
+               I0 = input$I0,
+               RcA = cbind(c(0L, 10L, 20L, 30L, 40L, 50L, 60L, 70L, 80L, 90L, 100L, 110L, 120L),
+                           c(0.84,0.84,0.84,0.84,0.84,0.84,0.84,0.88,0.88,1.0,1.0,1.0,1.0)),
+               RcT = cbind(
+                  c(12L, 16L, 20L, 24L, 28L, 32L, 36L, 40L),
+                  c(0, 0.42, 0.94, 0.94, 1.0, 0.85, 0.64, 0)),
+               RcOpt = input$RcOpt,
+               p = input$latent_period,
+               i = input$infectious_period,
+               Sx = 1000L,
+               a = input$aggregation,
+               RRS = input$RRS,
+               RRG = input$RRG)
+         }else{
+
          if (input$disease == "bb") {
             #cat("Bacterial Blight")
             epi_out<- predict_bacterial_blight(weather_dat(),
@@ -156,113 +289,41 @@ server <- function(input, output) {
             #cat("Tungro")
             epi_out<- predict_tungro(weather_dat(),
                                   emergence = input$startdate)
-         }
-         if (input$disease == "custom") {
-            #cat("Custom")
-               epi_out <- SEIR(
-                  wth = weather_dat(),
-                  emergence = input$startdate,
-                  onset = input$onset,
-                  duration = 120L,
-                  rhlim = 90L,
-                  rainlim = 5L,
-                  H0 = input$H0,
-                  I0 = input$I0,
-                  RcA = cbind(c(0L, 10L, 20L, 30L, 40L, 50L, 60L, 70L, 80L, 90L, 100L, 110L, 120L),
-                              c(0.84,0.84,0.84,0.84,0.84,0.84,0.84,0.88,0.88,1.0,1.0,1.0,1.0)),
-                  RcT = cbind(
-                     c(12L, 16L, 20L, 24L, 28L, 32L, 36L, 40L),
-                     c(0, 0.42, 0.94, 0.94, 1.0, 0.85, 0.64, 0)),
-                  RcOpt = input$RcOpt,
-                  p = input$latent_period,
-                  i = input$infectious_period,
-                  Sx = 1000L,
-                  a = input$aggregation,
-                  RRS = input$RRS,
-                  RRG = input$RRG)}
-
+         }}
          epi_out <- epi_out |>
             mutate(susceptible = sites - infectious - removed - latent)
          return(epi_out)
       })
 
-   output$custom_disease <- renderUI({
-      if (input$disease == "custom") {
-         tagList(
-         actionButton(inputId = "run_custom",
-                      label = "Custom Disease",
-                      status = "off",
-                      on = "Yes",
-                      off = "No"),
-            sliderInput("onset",
-                     "Onset of disease (days)",
-                     min = 1,
-                     max = 120,
-                     value = 30),
-         sliderInput("H0",
-                     "Initial Heathy sites",
-                     min = 1,
-                     max = 1000,
-                     value = 100),
-         sliderInput("I0",
-                     "Initial infectious sites",
-                     min = 1,
-                     max = 120,
-                     value = 1),
-         sliderInput("Tm_optim",
-                     "Optimum temperature for disease (C)",
-                     min = 0,
-                     max = 45,
-                     value = 24),
-         sliderInput("RH_optim",
-                     "Optimum Relative humidity for disease %",
-                     min = 30,
-                     max = 100,
-                     value = 90),
-         sliderInput("RcOpt",
-                     "Infection rate, sites per day",
-                     min = 0,
-                     max = 1,
-                     value = 0.5,
-                     step = 0.01),
-         sliderInput("latent_period",
-                     "Latent period (days)",
-                     min = 2,
-                     max = 30,
-                     value = 5),
-         sliderInput("infectious_period",
-                     "Infectious period (days)",
-                     min = 0,
-                     max = 100,
-                     value = 30),
-         sliderInput("aggregation",
-                     "Aggregation coeficient",
-                     min = 1,
-                     max = 20,
-                     value = 2.5,
-                     step = 0.1),
-         sliderInput("RRS",
-                     "Relative rate of senecence",
-                     min = 0,
-                     max = 0.2,
-                     value = 0.005,
-                     step = 0.001),
-         sliderInput("RRG",
-                     "Relative rate of Growth",
-                     min = 0,
-                     max = 1,
-                     value = 0.2,
-                     step = 0.01))
+   observeEvent(input$SEIR, {
+      cat(input$SEIR)
+      cat("Custom")
 
-      }else{
-         # it would be nuce here to return the default values for each of the
-         #   hard coded diseases options in epicrop
-         p("stuff goes here")
-      }
-   })
+      epicrop_out() <- SEIR(
+         wth = weather_dat(),
+         emergence = input$startdate,
+         onset = input$onset,
+         duration = 120L,
+         rhlim = 90L,
+         rainlim = 5L,
+         H0 = input$H0,
+         I0 = input$I0,
+         RcA = cbind(c(0L, 10L, 20L, 30L, 40L, 50L, 60L, 70L, 80L, 90L, 100L, 110L, 120L),
+                     c(0.84,0.84,0.84,0.84,0.84,0.84,0.84,0.88,0.88,1.0,1.0,1.0,1.0)),
+         RcT = cbind(
+            c(12L, 16L, 20L, 24L, 28L, 32L, 36L, 40L),
+            c(0, 0.42, 0.94, 0.94, 1.0, 0.85, 0.64, 0)),
+         RcOpt = input$RcOpt,
+         p = input$latent_period,
+         i = input$infectious_period,
+         Sx = 1000L,
+         a = input$aggregation,
+         RRS = input$RRS,
+         RRG = input$RRG)})
 
    # Disease plots ----------------------------------------------
-   output$disease_plot <- renderPlot({
+   output$disease_plot1 <-
+      output$disease_plot2 <-renderPlot({
 
       coloured_lines <- c(Healthy = "darkgreen",
                           Senesced = "gold",
@@ -317,7 +378,9 @@ server <- function(input, output) {
       grid.arrange(Intensity, sier_plot, ncol = 2)
    })
 
-   output$weather_plot <- renderPlot({
+   # Weather plots ----------------------------------------------
+   output$weather_plot1 <-
+      output$weather_plot2 <- renderPlot({
       w_dat <- weather_dat() |>
          filter(YYYYMMDD >= input$startdate,
                 YYYYMMDD <= input$startdate + 120)
